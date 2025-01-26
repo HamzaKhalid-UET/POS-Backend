149429bb7e9a4412ceba713c02af817c2a6371b8
hamzakhalid-UET <hamzakhalid.654@gmail.com>
2025-01-26 20:01:28 +0500

    formatted Code

--- src/routes/customer-routes.js
+++ src/routes/customer-routes.js
@@ -1,12 +1,6 @@
 import { addCustomer, deleteCustomer, getCustomer, getCustomerById, updateCustomer } from '@/app/controllers/customer-controller';
 import { addCustomerService } from '@/app/services/customer-service';
 import { AsyncTryCatch, router as customerRouter } from '@/utils';
-import { get } from 'mongoose';
-const { CustomerController } = require('../app/controllers');
-const { customerValidators } = require('../app/middlewares/validators');
-const { validateRequest } = require('../app/middlewares/handlers');
-
-
 customerRouter.post('/addcustomer', AsyncTryCatch(addCustomer));
 customerRouter.get('/getcustomer', AsyncTryCatch(getCustomer));
 customerRouter.post('/updatecustomer/:id', AsyncTryCatch(updateCustomer));
